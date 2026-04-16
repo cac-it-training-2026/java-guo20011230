749bb7aa61e2cc48d3f3235a0a7a28ffd7694e87
yupuguo <yupuguo@C010423081.zipangu.cac.co.jp>
2026-04-16 14:48:33 +0900

    Question05 2完了

--- java_comprehension_exercises_volume1/src/basic/question05/Question05_3.java
+++ java_comprehension_exercises_volume1/src/basic/question05/Question05_3.java
@@ -1,16 +1,42 @@
 package basic.question05;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-
-/**
- * 第8章 条件分岐
- */
+import java.io.InputStreamReader;
 
 public class Question05_3 {
-
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+		// 入力
+		String userDayInput = reader.readLine();
+
+		// 条件分岐（switch文）
+		switch (userDayInput) {
+		case "月曜日":
+			System.out.println("Monday");
+			break;
+		case "火曜日":
+			System.out.println("Tuesday");
+			break;
+		case "水曜日":
+			System.out.println("Wednesday");
+			break;
+		case "木曜日":
+			System.out.println("Thursday");
+			break;
+		case "金曜日":
+			System.out.println("Friday");
+			break;
+		case "土曜日":
+			System.out.println("Saturday");
+			break;
+		case "日曜日":
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("該当なし");
+		}
 	}
 }
